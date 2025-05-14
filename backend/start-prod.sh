1784e0a5485c7b35b9f5e3a0f1675464c465ea62
Philipp Goetz <home@p-goetz.de>
2025-05-14 21:59:57 +0200

    Updated CI/CD v8

--- backend/start-prod.sh
+++ backend/start-prod.sh
@@ -3,21 +3,8 @@
 # Run migrations
 python manage.py migrate
 
-# # Create superuser if it doesn't exist
-# python manage.py shell << END
-# from django.contrib.auth import get_user_model
-# import os
-# User = get_user_model()
-# if not User.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
-#     User.objects.create_superuser(
-#         username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-#         email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-#         password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-#     )
-#     print('Superuser created successfully')
-# else:
-#     print('Superuser already exists')
-# END
+# Create superuser if not already existing
+python manage.py createsu
 
 # Collect static files
 python manage.py collectstatic --noinput
