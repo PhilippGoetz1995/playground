f4a28e9226e26c7eb0fff9bae414a27f39abcc53
Philipp Goetz <home@p-goetz.de>
2025-08-17 20:12:42 +0200

    Changed location of version to package.json

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,27 +2,20 @@ import { useContext } from "react";
 import { LoginContext } from "../App";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import packageJson from '../../package.json';
+
 
 export default function Header() {
   const { loggedIn } = useContext(LoginContext);
 
-  const [ver, setVer] = useState(null);
-
-  useEffect(() => {
-    fetch(process.env.PUBLIC_URL + "/version.json", { cache: "no-store" })
-      .then(r => r.json())
-      .then(setVer)
-      .catch(() => {});
-  }, []);
-
-  if (!ver) return null;
-
   return (
+
+    
+
     <Navbar expand="lg" className="bg-secondary">
       <Container>
-        <Navbar.Brand shref="#home" className="text-white">
-          {/* TODO Move Version Number in env File */}
-          PG Playground v1.0.{ver.version} — {ver.builtAt}
+        <Navbar.Brand href="#home" className="text-white">
+          PG Playground v{packageJson.version}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
